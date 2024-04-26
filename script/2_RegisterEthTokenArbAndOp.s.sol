f1c1e48d670abbcbbf19a73c8ef70e8c207915a8
xiaoch05 <xiaoch2010@gmail.com>
2024-04-26 14:30:59 +0800

    Proposal 2: register op<>arb lnv3 eth token (#5)
    * Proposal 2: register op to arb eth
    
    * Proposal 2: register op <> arb eth
    
    * add interface
    
    * repair compile err
    
    * rename protocol name
    
    * renmae contract name
    
    * rename
    
    * rename

---
+++ script/2_RegisterEthTokenArbAndOp.s.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Base} from "./common/Base.sol";
+import {safeconsole} from "forge-std/safeconsole.sol";
+
+interface IHelixBridgeV3 {
+    struct TokenConfigure {
+        uint112 protocolFee;
+        uint112 penalty;
+        uint8 sourceDecimals;
+        uint8 targetDecimals;
+    }
+    struct TokenInfo {
+        TokenConfigure config;
+        uint32 index;
+        address sourceToken;
+        address targetToken;
+        uint256 protocolFeeIncome;
+    }
+    function registerTokenInfo(
+        uint256 _remoteChainId,
+        address _sourceToken,
+        address _targetToken,
+        uint112 _protocolFee,
+        uint112 _penalty,
+        uint8 _sourceDecimals,
+        uint8 _targetDecimals,
+        uint32 _index
+    ) external;
+    function getTokenKey(uint256 _remoteChainId, address _sourceToken, address _targetToken) pure external returns(bytes32);
+    function tokenInfos(bytes32 key) view external returns(TokenInfo memory);
+    function tokenIndexer(uint32 index) view external returns(bytes32);
+}
+
+contract RegisterEthTokenArbAndOp2 is Base {
+    address HelixBridge = 0xbA5D580B18b6436411562981e02c8A9aA1776D10;
+    uint256 OptimisticChainId = 10;
+    uint256 ArbitrumChainId = 42161;
+
+    function run() public sphinx {
+        uint256 remoteChainId = (block.chainid == OptimisticChainId ? ArbitrumChainId : OptimisticChainId); // arbitrum
+        address nativeAddress = address(0);
+        uint112 protocolFee = 1000000000000000;
+        uint112 penalty = 1500000000000000;
+        uint8 decimals = 18;
+        uint32 index = 1;
+        require(block.chainid == OptimisticChainId || block.chainid == ArbitrumChainId);
+        require(block.chainid != remoteChainId, "invalid chainid");
+        bytes32 tokenKey = IHelixBridgeV3(HelixBridge).getTokenKey(
+            remoteChainId,
+            nativeAddress,
+            nativeAddress
+        );
+        require(IHelixBridgeV3(HelixBridge).tokenInfos(tokenKey).index == 0, "this pair has been registered");
+        while (IHelixBridgeV3(HelixBridge).tokenIndexer(index) != bytes32(0)) {
+            index += 1;
+        }
+        IHelixBridgeV3(HelixBridge).registerTokenInfo(
+            remoteChainId,
+            nativeAddress,
+            nativeAddress,
+            protocolFee,
+            penalty,
+            decimals,
+            decimals,
+            index
+        );
+    }
+}
--- script/common/Base.sol
+++ script/common/Base.sol
@@ -17,6 +17,6 @@ contract Base is Sphinx, Script {
         sphinxConfig.threshold = 3;
         sphinxConfig.projectName = "Helix-DAO";
         // sphinxConfig.mainnets = ["polygon", "arbitrum", "optimism", "mantle"];
-        sphinxConfig.mainnets = ["polygon", "arbitrum", "optimism"];
+        sphinxConfig.mainnets = ["optimism", "arbitrum"];
     }
 }
