28fc2830dbeddf3a3c736b066d077cb3a0c183e7
xiaoch05 <xiaoch2010@gmail.com>
2024-04-28 13:32:51 +0800

    transfer ownership

--- script/common/Proposal.s.sol
+++ script/common/Proposal.s.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {RegisterEthTokenArbAndOp2} from "../2_RegisterEthTokenArbAndOp.s.sol";
+import {AcceptOwnership3} from "../3_AcceptOwnership.s.sol";
 
-contract Proposal is RegisterEthTokenArbAndOp2 {}
+contract Proposal is AcceptOwnership3 {}
