ee83521764588fb3dd5677091d6299d499fe5c6e
xiaoch05 <xiaoch2010@gmail.com>
2024-06-03 18:29:27 +0800

    upgrade msgport

--- script/common/Proposal.s.sol
+++ script/common/Proposal.s.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ConnectNetworkAndRegisterUsdt7} from "../7_ConnectNetworkAndRegisterUsdt.s.sol";
+import {UpgradeMsgport8} from "../8_UpgradeMsgport.s.sol";
 
-contract Proposal is ConnectNetworkAndRegisterUsdt7 {}
+contract Proposal is UpgradeMsgport8 {}
